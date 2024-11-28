df89a889cd3d57c98dbb7482ec19d2693ae90ee0
Oran Dennehy <24418943@studentmail.ul.ie>
2024-11-28 12:24:17 +0000

    Main Options
    Switch Case Unfinished

--- src/main/java/org/epiccalculator/Main.java
+++ src/main/java/org/epiccalculator/Main.java
@@ -1,14 +1,38 @@
 package org.epiccalculator;
 
 import java.util.Scanner;
-import static org.epiccalculator.Basic.calculate;
-
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a calculation: ");
-        String input = scanner.nextLine();
-        System.out.println(calculate(input));
+
+        System.out.println("Welcome students to your personal calculator\nTailored to your each and every need!");
+        System.out.println("Please select from the following options");
+        System.out.println("-----------------------------------------");
+
+        System.out.println("1 -> Basic Arithmetic");
+        System.out.println("2 -> Financial Calculator");
+        System.out.println("3 -> Unit Converter");
+        System.out.println("4 -> Binary/Hex/Decimal/Octal");
+        System.out.println("5 -> Mortgage Calculator");
+        System.out.println("7 -> Trigonometry");
+        System.out.println("8 -> Geometry");
+        System.out.println("9 -> Statistics");
+        System.out.println("10 -> Algebra");
+
+
+        System.out.println("\nEnter option number: ");
+
+        Scanner input = new Scanner(System.in);
+        int choice = input.nextInt(); // user choice variable to decide which calculator is appropriate
+
+
+        switch (choice) { //switch case to deal with user option and forwarding to correct class
+            case 1 :
+                ArithmeticUtils.options();
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + choice);
+        }
     }
 }
