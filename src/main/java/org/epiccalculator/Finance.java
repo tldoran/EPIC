d158a76d1f053ca725eedca37520f2f83e70a08c
mahdi <mphantomm10@gmail.com>
2024-11-28 11:44:57 +0000

    Added the method to the Finance class

--- src/main/java/org/epiccalculator/Finance.java
+++ src/main/java/org/epiccalculator/Finance.java
@@ -1,11 +1,14 @@
 package org.epiccalculator;
+import java.util.Scanner;
 
 public class Finance {
+    Scanner input = new Scanner(System.in);
     protected Scanner scanner;
 
     public Finance() {
-        this.Scanner = new Scanner(System.in);
+        this.input = new Scanner(System.in);
     }
 
-    public abstract void performCalculation();
+    public void performCalculation() {
+    }
 }
