5c8ee69f058e2f42cd230e093665a348a879bf8e
lucak0909 <lucak0909@gmail.com>
2024-11-28 11:40:26 +0000

    Added Algerbra class method outline

--- src/main/java/org/epiccalculator/Algerbra.java
+++ src/main/java/org/epiccalculator/Algerbra.java
@@ -1,4 +1,32 @@
 package org.epiccalculator;
 
 public class Algerbra {
+
+    public static double[] solvePolynomial(double[] coefficients) {
+        return coefficients;
+    }
+
+    public static double[] solveSimultaneous(double[][] matrix, double[] constants) {
+        return constants;
+    }
+
+    public static double[] graphPolynomial(double[] coefficients) {
+        return coefficients;
+    }
+
+    public static double[] derive(double[] coefficients) {
+        return coefficients;
+    }
+
+    public static double[] integrate(double[] coefficients) {
+        return coefficients;
+    }
+
+    public static double[] inflectionPoint(double[] coefficients) {
+        return coefficients;
+    }
+
+    public static double[] findMaxMin(double[] coefficients) {
+        return coefficients;
+    }
 }
